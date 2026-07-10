e3436a0bba2a3a09d243faeb4950bdc5372cf83e
mansi-dkakad123 <mansidhakad08@gmail.com>
2026-07-10 22:15:19 +0530

    fix: update proxy and allowed hosts for render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: [
+      "aeromind-drone-simulator-1.onrender.com"
+    ],
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://aeromind-drone-simulator.onrender.com",
         changeOrigin: true,
       },
     },
